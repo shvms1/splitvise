c9bf761c3037d9703fe93f5853a698cfabb2ee95
shivam singhal <shvmsinghal07@gmail.com>
2020-01-03 07:47:29 +0530

    Added more changes

--- splitwise_main.c
+++ splitwise_main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define ADD_ITEM_TO_BOUGHT 0
 #define ADD_ITEM_YOU_BOUGHT 1
+#define QUIT_PROGRAM 2
 
 int main()
 {
@@ -9,24 +12,27 @@ int main()
 	char item[50] = {0};
     char person_name[50] = {0};
 	int quantity = 0;
-	printf("********************welcome to splitwise********************");
+	printf("********************welcome to splitwise********************\n");
 	do
 	{
-		printf("\t\tselect option\n");
-		printf("\t\t To add item to bought, press 0\n");
-		printf("\t\t To add Expenses (items you have bought), press 1\n");
+		printf("select option\n");
+		printf("To add item to bought, press 0\n");
+		printf("To add Expenses (items you have bought), press 1\n");
+		printf("to quit, press 2\n");
 		scanf("%d", &ch);
 
 		switch(ch)
 		{
-			case ADD_ITEM_TO_BOUGHT: 
-				printf("please mention items\n");
+			case ADD_ITEM_TO_BOUGHT:
 				do
                 {
-                    scanf("%s\n", item);
+                    printf("please mention items\n");
+                    memset(item, 0, sizeof(item));
+                    scanf("%s", item);
 				    printf("please mention quantity\n");
+
 				    scanf("%d", &quantity);
-                    printf("want to end more? press 1 otherwise add 0");
+                    printf("want to end more? press 1 otherwise add 0\n");
                     scanf("%d", &choice);
                 }
                 while(choice != 0);
@@ -34,13 +40,15 @@ int main()
 			case ADD_ITEM_YOU_BOUGHT:
 				printf("add person to split with\n");
                 do
-                {                    
+                {
                     scanf("%s", person_name );
-                    printf("want to add more person? press 1 else press 0");
+                    printf("want to add more person? press 1 else press 0\n");
                     scanf("%d", &choice);
                 }
                 while( choice != 0 );
 				break;
+            case QUIT_PROGRAM:
+                break;
             default:
                 printf("wrong choice\n");
                 break;
